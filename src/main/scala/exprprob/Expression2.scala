2a688928acd1c01923c2c1c403c7c14f1df19dcd
Brian Howard <bhoward@depauw.edu>
2024-09-21 20:33:43 -0400

    Move givens into companion objects

--- src/main/scala/exprprob/Expression2.scala
+++ src/main/scala/exprprob/Expression2.scala
@@ -15,45 +15,46 @@ case class Add[L <: Expr, R <: Expr](left: L, right: R) extends Expr
 trait Eval[E <: Expr]:
   def eval(e: E): Int
 
-given Eval[Const] with
-  def eval(c: Const): Int = c.value
+object Eval:
+  given Eval[Const] with
+    def eval(c: Const): Int = c.value
 
-given [L <: Expr : Eval, R <: Expr : Eval]: Eval[Add[L, R]] with
-  def eval(a: Add[L, R]): Int = {
-    val leftEval = summon[Eval[L]].eval
-    val rightEval = summon[Eval[R]].eval
-    leftEval(a.left) + rightEval(a.right)
-  }
+  given [L <: Expr : Eval, R <: Expr : Eval]: Eval[Add[L, R]] with
+    def eval(a: Add[L, R]): Int = {
+      val leftEval = summon[Eval[L]].eval
+      val rightEval = summon[Eval[R]].eval
+      leftEval(a.left) + rightEval(a.right)
+    }
 
 // Add neg variant
 
 case class Neg[E <: Expr](e: E) extends Expr
 
-given [T <: Expr : Eval]: Eval[Neg[T]] with
-  def eval(a: Neg[T]): Int = {
-    val argEval = summon[Eval[T]].eval
-    -argEval(a.e)
-  }
+object Neg:
+  given [T <: Expr : Eval]: Eval[Neg[T]] with
+    def eval(a: Neg[T]): Int = {
+      val argEval = summon[Eval[T]].eval
+      -argEval(a.e)
+    }
 
 // Add show operation (as an extension method, for variety)
 
 trait Show[E <: Expr]:
-  extension (e: E) def show: String
+  def show(e: E): String
 
-given Show[Const] with
-  extension (c: Const) def show: String = c.value.toString
+object Show:
+  given Show[Const] with
+    def show(c: Const): String = c.value.toString
 
-given [L <: Expr : Show, R <: Expr : Show]: Show[Add[L, R]] with
-  extension (a: Add[L, R])
-    def show: String = {
+  given [L <: Expr : Show, R <: Expr : Show]: Show[Add[L, R]] with
+    def show(a: Add[L, R]): String = {
       val leftShow = summon[Show[L]].show
       val rightShow = summon[Show[R]].show
       s"(${leftShow(a.left)} + ${rightShow(a.right)})"
     }
 
-given [E <: Expr : Show]: Show[Neg[E]] with
-  extension (a: Neg[E])
-    def show: String = {
+  given [E <: Expr : Show]: Show[Neg[E]] with
+    def show(a: Neg[E]): String = {
       val argShow = summon[Show[E]].show
       s"-${argShow(a.e)}"
     }
@@ -62,16 +63,16 @@ given [E <: Expr : Show]: Show[Neg[E]] with
 
 case class Mul[L <: Expr, R <: Expr](left: L, right: R) extends Expr
 
-given [L <: Expr : Eval, R <: Expr : Eval]: Eval[Mul[L, R]] with
-  def eval(t: Mul[L, R]): Int = {
-    val leftEval = summon[Eval[L]].eval
-    val rightEval = summon[Eval[R]].eval
-    leftEval(t.left) * rightEval(t.right)
-  }
+object Mul:
+  given [L <: Expr : Eval, R <: Expr : Eval]: Eval[Mul[L, R]] with
+    def eval(t: Mul[L, R]): Int = {
+      val leftEval = summon[Eval[L]].eval
+      val rightEval = summon[Eval[R]].eval
+      leftEval(t.left) * rightEval(t.right)
+    }
 
-given [L <: Expr : Show, R <: Expr : Show]: Show[Mul[L, R]] with
-  extension (a: Mul[L, R])
-    def show: String = {
+  given [L <: Expr : Show, R <: Expr : Show]: Show[Mul[L, R]] with
+    def show(a: Mul[L, R]): String = {
       val leftShow = summon[Show[L]].show
       val rightShow = summon[Show[R]].show
       s"(${leftShow(a.left)} * ${rightShow(a.right)})"
@@ -81,6 +82,7 @@ given [L <: Expr : Show, R <: Expr : Show]: Show[Mul[L, R]] with
 
 @main def exprDemo2: Unit = {
   def eval[E <: Expr: Eval](e: E): Int = summon[Eval[E]].eval(e)
+  extension [E <: Expr: Show](e: E) def show: String = summon[Show[E]].show(e)
 
   val e = Neg(Add(Const(15), Neg(Mul(Const(19), Const(3)))))
   println(eval(e))
@@ -96,7 +98,7 @@ given [L <: Expr : Show, R <: Expr : Show]: Show[Mul[L, R]] with
   println(x)
   // However, the following are still not possible, because just having
   // a value of type Expr does not give us access at compile time to the
-  // appropriate evidence to resolve which instance it is
+  // appropriate evidence to resolve which instance it is:
   // println(eval(x))
   // println(x.show)
 }
